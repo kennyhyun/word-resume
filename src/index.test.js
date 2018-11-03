185c5fd16487c6b40c9a020e5da97f578ef777ff
Kenny Yeo <kenny@yeoyou.net>
2018-11-04 01:12:32 +1100

    remove dates from doc for snapshot test

--- src/index.test.js
+++ src/index.test.js
@@ -4,5 +4,9 @@ const generateDocument = require('./');
 it('should match snapshot', () => {
   const source = generateSeed('jest test context');
   const doc = generateDocument(source);
+  const [Created, Modified] = doc.coreProperties.root.slice(-2);
+  Created.root.pop();
+  Modified.root.pop();
+  
   expect(doc).toMatchSnapshot();
 });
