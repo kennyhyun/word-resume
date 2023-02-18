9a8a8de57e2d51e2f318b480f2db0fbc94891175
Kenny Yeo <kenny.yeo@whitech.com.au>
2023-02-18 16:07:33 +0000

    fix seeders and test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,28 @@
-const generateDocument = require("../");
-const generateSeed = require("../seeder");
+import { promises as fsp } from 'fs';
+import { Packer } from 'docx';
+import AdmZip from 'adm-zip';
 
-it("should match snapshot", () => {
-  const now = new Date("2018-11-01");
-  const source = generateSeed("jest test context", now);
-  const doc = generateDocument(source, undefined, now);
-  const [Created, Modified] = doc.coreProperties.root.slice(-2);
-  Created.root.pop();
-  Modified.root.pop();
+const generateDocument = require('../');
+const generateSeed = require('../seeder');
 
-  expect(doc).toMatchSnapshot();
+describe('generateDocument', () => {
+  it('should match snapshot', async () => {
+    const now = new Date('2018-11-01');
+    const source = generateSeed('jest test context', now);
+
+    const doc = generateDocument(source, undefined, now);
+    const [Created, Modified] = doc.coreProperties.root.slice(-2);
+    Created.root.pop();
+    Modified.root.pop();
+
+    const packer = new Packer();
+    const docBuffer = await packer.toBuffer(doc);
+    const filename = './test.docx';
+    await fsp.writeFile(filename, docBuffer);
+    const zip = AdmZip(filename);
+    const docEntry = zip
+      .getEntries()
+      .find(({ entryName }) => entryName === 'word/document.xml');
+    expect(zip.readFile(docEntry).toString()).toMatchSnapshot();
+  });
 });
--- src/__tests__/seeder.test.js
+++ src/__tests__/seeder.test.js
@@ -2,10 +2,8 @@ import generateSeed from '../seeder';
 
 describe('generateSeed', () => {
   it('should seed sample yml', () => {
-    const now = new Date('20i23-02-01');
+    const now = new Date('2018-11-01');
     const source = generateSeed('jest test context', now);
     expect(source).toMatchSnapshot();
   });
 });
-
-
